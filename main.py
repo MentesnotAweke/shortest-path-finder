ef15de85716800b60537e648ceab54e8136a562d
mintesnot <mentesnotaa@gmail.com>
2025-08-20 16:37:29 +0300

    Add maze solver implementation with curses interface

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("hello world!")
-print("This is a simple Python script.")
+import curses
+from curses import wrapper
+import queue
+import time
+
+maze = maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Maze Solver")
+    stdscr.refresh()
+    stdscr.getch()  # Wait for user input to start
+
+wrapper(main)
