d4df1c0b72e8143c1cd7f66703661de210fddb92
mintesnot <mentesnotaa@gmail.com>
2025-08-20 16:50:43 +0300

    Refactor maze printing logic into a separate function

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ maze = maze = [
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
+def print_maze(stdscr, maze, path=None):
+    GREEN = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, cell in enumerate(row):
+            stdscr.addstr(i, j*2, cell, GREEN)
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    green_and_black = curses.color_pair(1)
     stdscr.clear()
-    stdscr.addstr(0, 0, "Maze Solver")
+    print_maze(stdscr, maze)
     stdscr.refresh()
     stdscr.getch()  # Wait for user input to start
 
